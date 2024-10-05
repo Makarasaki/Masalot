085fb3e194dd79cc6e0cba3f1c49b4eeb8d3fd1c
Maksymilian Leder <makaleder@gmail.com>
2024-10-05 18:31:54 +0200

    unnecessary

--- data_preparation/SQL/create_table_with_N_random_records.sql
+++ data_preparation/SQL/create_table_with_N_random_records.sql
@@ -1,8 +1,8 @@
-Step 1: Create the new table big_db
-CREATE TABLE big_db_100_000 AS SELECT * FROM evaluations WHERE 0;
+-- Step 1: Create the new table big_db
+CREATE TABLE one_program_test AS SELECT * FROM evaluations WHERE 0;
 
 -- Step 2: Insert 1000 random rows into big_db
-INSERT INTO big_db_100_000
+INSERT INTO one_program_test
 SELECT * FROM evaluations
 ORDER BY RANDOM()
-LIMIT 100000;
+LIMIT 1000;
